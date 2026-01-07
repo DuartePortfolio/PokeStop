b34741a543693a2d29c7978f0f8a3ff77f49bbd8
DuartePortfolio <40230362@esmad.ipp.pt>
2026-01-07 17:36:15 +0000

    migrated from static data to mysql database for user service

--- user-service/app.js
+++ user-service/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 const userRoutes = require('./routes/userRoutes');
+const sequelize = require('./services/sequelize');
+const User = require('./models/User');
 
 app.use(express.json());
 app.use('/users', userRoutes);
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`User service running on port ${PORT}`));
+sequelize.sync().then(() => {
+	const PORT = process.env.PORT || 3001;
+	app.listen(PORT, () => console.log(`User service running on port ${PORT}`));
+});
