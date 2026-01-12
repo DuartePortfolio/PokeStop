f265ea7ca59f3a3b2771b81cb0f432418d1447f4
DuartePortfolio <40230362@esmad.ipp.pt>
2026-01-12 15:48:33 +0000

    added mongodb database for collections service

--- user-service/services/sequelize.js
+++ user-service/services/sequelize.js
@@ -1,9 +1,14 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('pokestop_user_service', 'root', 'enter', {
-  host: 'localhost',
-  dialect: 'mysql',
-  logging: false
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME || 'pokestop_users_db',
+  process.env.DB_USER || 'root',
+  process.env.DB_PASSWORD || 'enter',
+  {
+    host: process.env.DB_HOST || 'db',
+    dialect: 'mysql',
+    logging: false
+  }
+);
 
 module.exports = sequelize;
