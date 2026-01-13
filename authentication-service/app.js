6a459f1d76a2336ba0687988eac9dcfccb424680
DuartePortfolio <40230362@esmad.ipp.pt>
2026-01-13 22:42:27 +0000

    fixed docker swarm

--- authentication-service/app.js
+++ authentication-service/app.js
@@ -3,4 +3,5 @@ const axios = require("axios"); // we'll use this to call the review service
 const app = express();
 
 app.use(express.json());
-
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Authentication service running on port ${PORT}`));
